63ffe5867bc366bcb2add8320f14dd9565e26acb
Hixie <github.com@spam.hixie.ch>
2019-05-20 11:22:23 -0700

    More tweaks based on writing run_tests.dart

--- scripts/verify_tests_on_master.sh
+++ scripts/verify_tests_on_master.sh
@@ -6,4 +6,4 @@ flutter/bin/flutter doctor -v
 
 # Now run the tests a bunch of times to try to find flakes (tests that sometimes pass
 # even though they should be failing).
-flutter/bin/cache/dart-sdk/bin/dart flutter/dev/customer_testing/run_tests.dart --repeat 15 registry/
+flutter/bin/cache/dart-sdk/bin/dart flutter/dev/customer_testing/run_tests.dart --repeat=15 --skip-template registry/*.test
