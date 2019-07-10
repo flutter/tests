07d6f8223af6b7867dd22c044fb9aea88f838c5b
Hixie <github.com@spam.hixie.ch>
2019-07-10 12:28:16 -0700

    Update documentation

--- scripts/verify_tests_on_master.sh
+++ scripts/verify_tests_on_master.sh
@@ -4,6 +4,12 @@ set -ex
 git clone https://github.com/flutter/flutter.git
 flutter/bin/flutter doctor -v
 
+export PATH="$PATH":`pwd`/flutter/bin:`pwd`/flutter/bin/cache/dart-sdk/bin
+
+cd flutter/dev/customer_testing
+pub get
+cd ../../..
+
 # Now run the tests a bunch of times to try to find flakes (tests that sometimes pass
 # even though they should be failing).
-flutter/bin/cache/dart-sdk/bin/dart flutter/dev/customer_testing/run_tests.dart --repeat=15 --skip-template registry/*.test
+dart flutter/dev/customer_testing/run_tests.dart --repeat=15 --skip-template registry/*.test
