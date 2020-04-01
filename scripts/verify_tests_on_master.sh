3ba3b180fb3cc3cf6d7078da05ba3b6f02ba93a1
Casey Hillers <chillers@google.com>
2020-03-31 18:00:55 -0700

    Add sharding to posix scripts

--- scripts/verify_tests_on_master.sh
+++ scripts/verify_tests_on_master.sh
@@ -1,7 +1,30 @@
+#!/bin/bash
 set -ex
 
+# Default values.
+SHARDS=1
+SHARD_INDEX=0
+
+# Parse given command for variables.
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -n|--shards)
+    SHARDS="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -i|--shard-index)
+    SHARD_INDEX="$2"
+    shift # past argument
+    shift # past value
+    ;;
+esac
+done
+
 # Fetch Flutter.
-git clone https://github.com/flutter/flutter.git
+#git clone https://github.com/flutter/flutter.git
 flutter/bin/flutter doctor -v
 
 export PATH="$PATH":`pwd`/flutter/bin:`pwd`/flutter/bin/cache/dart-sdk/bin
@@ -12,4 +35,4 @@ cd ../../..
 
 # Now run the tests a bunch of times to try to find flakes (tests that sometimes pass
 # even though they should be failing).
-dart flutter/dev/customer_testing/run_tests.dart --repeat=15 --skip-template registry/*.test
+dart flutter/dev/customer_testing/run_tests.dart --shards $SHARDS --shard-index $SHARD_INDEX --repeat=15 --skip-template --verbose registry/*.test
