ec07a13e631548598c8eece92fc59749fb3748a7
Casey Hillers <caseyhillers@gmail.com>
2020-03-31 20:19:37 -0700

    make windows verbose

--- scripts/verify_tests_on_master.bat
+++ scripts/verify_tests_on_master.bat
@@ -34,7 +34,7 @@ CD ..\..\..
 REM Now run the tests a bunch of times to try to find flakes (tests that sometimes pass
 REM even though they should be failing).
 @ECHO.
-CALL dart flutter\dev\customer_testing\run_tests.dart --repeat=15 --skip-template --shards %SHARDS% --shard-index %SHARD_INDEX% registry/*.test || GOTO :END
+CALL dart flutter\dev\customer_testing\run_tests.dart --repeat=15 --skip-template --shards %SHARDS% --shard-index %SHARD_INDEX% --verbose registry/*.test || GOTO :END
 @ECHO ON
 
 @ECHO.
