3f0e37cd7a21688298ff52b55201bc0d2df37792
Casey Hillers <caseyhillers@gmail.com>
2020-04-02 14:54:27 -0700

    Switch batch comments to :: (#33)

--- scripts/verify_tests_on_master.bat
+++ scripts/verify_tests_on_master.bat
@@ -1,10 +1,10 @@
 @ECHO ON
 
-REM Default values.
+:: Default values.
 SET SHARDS=1
 SET SHARD_INDEX=0
 
-REM Parse the arguments for variables.
+:: Parse the arguments for variables.
 :parse
 IF "%1"=="" GOTO endparse
 IF "%1"=="--shards" (
@@ -18,7 +18,7 @@ SHIFT
 GOTO parse
 :endparse
 
-REM Fetch Flutter.
+:: Fetch Flutter.
 git clone https://github.com/flutter/flutter.git || GOTO :END
 CALL flutter\bin\flutter doctor -v || GOTO :END
 @ECHO ON
@@ -33,8 +33,8 @@ CALL pub get || GOTO :END
 @ECHO ON
 CD ..\..\..
 
-REM Now run the tests a bunch of times to try to find flakes (tests that sometimes pass
-REM even though they should be failing).
+:: Now run the tests a bunch of times to try to find flakes (tests that sometimes pass
+:: even though they should be failing).
 @ECHO.
 CALL dart flutter\dev\customer_testing\run_tests.dart --repeat=15 --skip-template --shards %SHARDS% --shard-index %SHARD_INDEX% --verbose registry/*.test || GOTO :END
 @ECHO ON
